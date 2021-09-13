e4322de0774dbf352475cb316ea981edb711b271
edkeric <ed.keric@gmail.com>
2021-09-13 12:05:35 +0200

    facebook google

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { Route, useLocation } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import { Container } from 'semantic-ui-react'
+import AccountPage from '../../features/auth/AccountPage'
 import EventDashboard from '../../features/events/eventDashboard/EventDashboard'
 import EventDetailsPage from '../../features/events/eventDetails/EventDetailsPage'
 import EventForm from '../../features/events/eventForm/EventForm'
@@ -10,9 +12,13 @@ import NavBar from '../../features/nav/NavBar'
 import Sandbox from '../../features/reducers/Sandbox'
 import ErrorComponent from '../common/errors/ErrorComponent'
 import ModalManager from '../common/modals/ModalManager'
+import LoadingComponent from './LoadingComponent'
 
 export default function App() {
   const { key } = useLocation()
+  const { initialized } = useSelector((state) => state.async)
+
+  if (!initialized) return <LoadingComponent content='Loading app...' />
   return (
     <>
       <ModalManager />
@@ -33,6 +39,7 @@ export default function App() {
                 key={key}
               />
               <Route path='/error' component={ErrorComponent} />
+              <Route path='/account' component={AccountPage} />
             </Container>
           </>
         )}
--- src/features/nav/SignOut.jsx
+++ src/features/nav/SignOut.jsx
@@ -14,6 +14,7 @@ export default function SignOut({ setAuth }) {
         content='Login'
       />
       <Button
+        onClick={() => dispatch(openModal({ modalType: 'RegisterForm' }))}
         basic
         inverted
         content='Register'
